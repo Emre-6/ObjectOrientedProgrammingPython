770da31966a7770aab862eb747b32027fb88d948
Özgün Emre Omurtak <76629162+Emre-6@users.noreply.github.com>
2023-08-15 11:12:58 +0300

    Add files via upload

---
+++ QuizApp.py
@@ -0,0 +1,8 @@
+q1=Question("Which is the best programming language?",["Python","C#","Java","Dart"],"Python")
+q2=Question("Which is the most popular programming language?",["Python","Java","C#","Dart"],"Python")
+q3=Question("Which is the best valuable programming language?",["Python","Java","Dart","C#"],"Python")
+
+questions=[q1,q2,q3]
+
+
+
